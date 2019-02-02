8a6e411717066816854429537847278689b822b0
Benjamin Brandtner <37274870+BenjaminBrandtner@users.noreply.github.com>
2019-02-02 12:51:03 +0100

    Initial Commit
    Retrieve todos and print them to console

--- HabiticaSimpleToDo/Program.cs
+++ HabiticaSimpleToDo/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net;
 
 namespace HabiticaSimpleToDo
 {
@@ -9,6 +8,33 @@ namespace HabiticaSimpleToDo
     {
         static void Main(string[] args)
         {
+            String url = "https://habitica.com/api/v3/tasks/user?type=todos";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers["x-api-user"] = Properties.settings.Default.userID;
+            request.Headers["x-api-key"] = Properties.settings.Default.apiToken;
+
+            try
+            {
+                //Send the Request
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                //Read some headers from response
+                Console.WriteLine("Status: " + response.StatusCode);
+                Console.WriteLine("ContentType: " + response.ContentType);
+
+                //Output data
+                //TODO: Parse json instead of just printing it to console.
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                Console.WriteLine(reader.ReadToEnd());
+
+                response.Close();
+            }
+            catch (WebException)
+            {
+                throw;
+            }
         }
     }
 }
