3a7fa9787b93c91d885c7e49b3ac6a240340ee7e
Benjamin Brandtner <37274870+BenjaminBrandtner@users.noreply.github.com>
2019-02-16 19:08:48 +0100

    Created Classes to Deserialze HabiticaTodos into

---
+++ HabiticaSimpleToDo/HabiticaChecklistItem.cs
@@ -0,0 +1,20 @@
+﻿namespace HabiticaSimpleToDo
+{
+    internal class HabiticaChecklistItem
+    {
+        private readonly string id;
+        private string text;
+        private bool completed;
+
+        public HabiticaChecklistItem(string id, string text, bool completed)
+        {
+            this.id = id;
+            this.text = text;
+            this.completed = completed;
+        }
+
+        public string Id => id;
+        public string Text { get => text; set => text = value; }
+        public bool Completed { get => completed; set => completed = value; }
+    }
+}
---
+++ HabiticaSimpleToDo/HabiticaTodo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HabiticaSimpleToDo
+{
+    class HabiticaTodo : ITodo
+    {
+        private String id;
+        private String text;
+        private String notes;
+        private float value;
+        private bool completed;
+        private float priority;
+        private DateTime date;
+
+        private List<HabiticaChecklistItem> checklist;
+        private bool collapseChecklist;
+
+        private DateTime createdAt;
+        private DateTime updatedAt;
+
+        public string Id { get => id; set => id = value; }
+        public string Text { get => text; set => text = value; }
+        public string Notes { get => notes; set => notes = value; }
+        public float Value { get => value; set => this.value = value; }
+        public bool Completed { get => completed; set => completed = value; }
+        public float Priority { get => priority; set => priority = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public bool CollapseChecklist { get => collapseChecklist; set => collapseChecklist = value; }
+        public DateTime CreatedAt { get => createdAt; set => createdAt = value; }
+        public DateTime UpdatedAt { get => updatedAt; set => updatedAt = value; }
+        public List<HabiticaChecklistItem> Checklist { get => checklist; set => checklist = value; }
+
+        public void checkOff()
+        {
+            completed = true;
+        }
+        public override String ToString()
+        {
+            return text + " (" + notes + ")";
+        }
+    }
+
+    public class Priority
+    {
+        public const float Trivial = 0.1f;
+        public const float Easy = 1;
+        public const float Normal = 1.5f;
+        public const float Hard = 2;
+    }
+
+}
