dfebe888f5879b7101fefe68ce97fb0e85d2a0c8
Benjamin Brandtner <37274870+BenjaminBrandtner@users.noreply.github.com>
2019-07-07 22:09:46 +0200

    Implemented loading indicator in options menu

--- ViewModel/VMOptions.cs
+++ ViewModel/VMOptions.cs
@@ -1,73 +1,84 @@
-﻿using Backend;
-using System;
+﻿using System;
 using System.ComponentModel;
 using System.Windows.Input;
+using Backend;
 
 namespace ViewModel
 {
-    public class VMOptions : INotifyPropertyChanged
-    {
-        private ConfigManager configManager;
-        private dynamic config;
+	public class VMOptions : INotifyPropertyChanged
+	{
+		private readonly ConfigManager configManager;
+		private readonly dynamic config;
 
-        private string errorMessage;
-        private string successMessage;
-        private ICommand validateCommand;
-        public event PropertyChangedEventHandler PropertyChanged;
+		private string errorMessage;
+		private string successMessage;
+		private bool busy;
 
-        public VMOptions()
-        {
-            configManager = new ConfigManager();
-            config = configManager.Read();
+		public event PropertyChangedEventHandler PropertyChanged;
 
-            ValidateCommand = new UserCommand(new Action<object>(ValidateCredentials));
-        }
+		public VMOptions()
+		{
+			configManager = new ConfigManager();
+			config = configManager.Read();
 
-        public ICommand ValidateCommand { get => validateCommand; set => validateCommand = value; }
-        public string UserID { get => config.UserID; set => config.UserID = value; }
-        public string ApiToken { get => config.ApiToken; set => config.ApiToken = value; }
-        public string ErrorMessage
-        {
-            get => errorMessage;
-            set { errorMessage = value; OnPropertyChanged(new PropertyChangedEventArgs("ErrorMessage")); }
-        }
-        public string SuccessMessage
-        {
-            get => successMessage; set
-            { successMessage = value; OnPropertyChanged(new PropertyChangedEventArgs("SuccessMessage")); }
-        }
+			ValidateCommand = new UserCommand(ValidateCredentials);
+		}
 
-        public void OnPropertyChanged(PropertyChangedEventArgs e)
-        {
-            PropertyChanged?.Invoke(this, e);
-        }
+		//Properties
+		public string UserID { get => config.UserID; set => config.UserID = value; }
+		public string ApiToken { get => config.ApiToken; set => config.ApiToken = value; }
+		public string ErrorMessage
+		{
+			get => errorMessage;
+			set { errorMessage = value; OnPropertyChanged(new PropertyChangedEventArgs("ErrorMessage")); }
+		}
+		public string SuccessMessage
+		{
+			get => successMessage;
+			set { successMessage = value; OnPropertyChanged(new PropertyChangedEventArgs("SuccessMessage")); }
+		}
+		public bool Busy
+		{
+			get => busy;
+			set { busy = value; OnPropertyChanged(new PropertyChangedEventArgs("Busy")); }
+		}
+		//Events
+		public void OnPropertyChanged(PropertyChangedEventArgs e) { PropertyChanged?.Invoke(this, e); }
+		//Commands
+		public ICommand ValidateCommand { get; set; }
 
-        private async void ValidateCredentials(object obj)
-        {
-            ErrorMessage = "";
-            SuccessMessage = "";
 
-            try
-            {
-                HabiticaClient client = HabiticaClient.GetInstance(config);
-                await client.TestConnection();
+		private async void ValidateCredentials(object obj)
+		{
+			ErrorMessage = "";
+			SuccessMessage = "";
+			Busy = true;
 
-                SuccessMessage = "Credentials validated successfully";
+			try
+			{
+				HabiticaClient client = HabiticaClient.GetInstance(config);
+				await client.TestConnection();
 
-                configManager.Write(config);
-            }
-            catch (NoCredentialsException)
-            {
-                ErrorMessage = "One of the required fields is empty";
-            }
-            catch (WrongCredentialsException e)
-            {
-                ErrorMessage = e.Message;
-            }
-            catch (UnsuccessfulException e)
-            {
-                ErrorMessage = e.Message;
-            }
-        }
-    }
+				SuccessMessage = "Credentials validated successfully";
+
+				configManager.Write(config);
+			}
+			catch (NoCredentialsException)
+			{
+				ErrorMessage = "One of the required fields is empty";
+			}
+			catch (WrongCredentialsException e)
+			{
+				ErrorMessage = e.Message;
+			}
+			catch (UnsuccessfulException e)
+			{
+				ErrorMessage = e.Message;
+			}
+			finally
+			{
+				Busy = false;
+			}
+		}
+	}
 }
